e4b6cc96d3fa68f9aa92b5bf94c675273f85eddf
kokolo4442 <159828248+kokolo4442@users.noreply.github.com>
2025-02-13 21:45:23 +0100

    Add files via upload

---
+++ Walentynka/script.js
@@ -0,0 +1,51 @@
+(async function checkForUpdates() {
+    const currentVersion = "1.0";
+    const versionUrl = ""; 
+
+    try {
+        const response = await fetch(versionUrl);
+        if (!response.ok) {
+            console.warn("Could not fetch version information.");
+            return;
+        }
+        const data = await response.json();
+        const latestVersion = data.version;
+        const updateMessage = data.updateMessage;
+
+        if (currentVersion !== latestVersion) {
+            alert(updateMessage);
+        } else {
+            console.log("You are using the latest version.");
+        }
+    } catch (error) {
+        console.error("Error checking for updates:", error);
+    }
+})();
+
+const messages = [
+    "Jesteś pewna?",
+    "Tak naprawde pewna",
+    "Na sto procent?",
+    "No dawaj",
+    "Pomyśl o tym!",
+    "Tak naprawde pomyśl",
+    "Jesteś pewna na milion procent?",
+    "Dobra przestane",
+    "Żartowałem, no dawaj!❤️",
+    "❤️"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
